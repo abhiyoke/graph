08dc349fdbfa4f7baad6b52ebacd4ca5523057ac
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2023-04-17 00:46:30 +0530

    Create 9c.course_schedule1.cpp

---
+++ 9c.course_schedule1.cpp
@@ -0,0 +1,65 @@
+//https://practice.geeksforgeeks.org/problems/prerequisite-tasks/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=prerequisite-tasks
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int n, vector<pair<int, int> >& prerequisites) {
+	    // Code here
+	    vector<int>adj[n];
+	    for(auto it:prerequisites){
+	        adj[it.first].push_back(it.second);
+	    }
+	    int indegree[n]={0};
+	    for(int i=0;i<n;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int>q;
+	    vector<int>topo;
+	    for(int i=0;i<n;i++){
+	        if(indegree[i]==0) q.push(i);
+	    }
+	    while(!q.empty()){
+	        int node=q.front();
+	        topo.push_back(node);
+	        q.pop();
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0) q.push(it);
+	        }
+	    }
+	    if(topo.size()==n) return true;
+    else return false;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
